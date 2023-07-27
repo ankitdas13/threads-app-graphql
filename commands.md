9cb502cb70c9e023d801d779bd56b3c743d8b03c
ankitdas13 <ankit.das@razorpay.com>
2023-07-28 04:02:20 +0530

    prisma setup

---
+++ commands.md
@@ -0,0 +1,68 @@
+## Show all running container
+docker ps
+
+## Access container
+docker exec -it <Container Id> bash
+
+## user name
+su postgres
+
+## list of db
+
+postgres=#  \l
+
+output : 
+
+ List of databases
+   Name    |  Owner   | Encoding |  Collate   |   Ctype    | ICU Locale | Locale Provider |   Access privileges
+-----------+----------+----------+------------+------------+------------+-----------------+-----------------------
+ postgres  | postgres | UTF8     | en_US.utf8 | en_US.utf8 |            | libc            |
+ template0 | postgres | UTF8     | en_US.utf8 | en_US.utf8 |            | libc            | =c/postgres          +
+           |          |          |            |            |            |                 | postgres=CTc/postgres
+ template1 | postgres | UTF8     | en_US.utf8 | en_US.utf8 |            | libc            | =c/postgres          +
+           |          |          |            |            |            |                 | postgres=CTc/postgres
+ threads   | postgres | UTF8     | en_US.utf8 | en_US.utf8 |            | libc            |
+
+## to connect db
+
+postgres=#  \c threads
+
+output : You are now connected to database "threads" as user "postgres".
+
+## list of relation
+threads=# \d
+
+output:
+
+               List of relations
+ Schema |        Name        | Type  |  Owner   
+--------+--------------------+-------+----------
+ public | _prisma_migrations | table | postgres
+ public | users              | table | postgres
+
+
+## expanded display cmd
+threads=# \x
+Expanded display is on.
+
+
+threads=# select * from users;
+-[ RECORD 1 ]-----+-------------------------------------
+id                | 6265a14a-6573-4300-b829-0a20a910c5f0
+first_name        | ankit
+last_name         | das
+profile_image_url |
+email             | ankitdas@example.com
+password          | 1234
+salt              | testing_salt
+
+
+
+
+
+
+
+
+## prisma schema migration
+
+npx prisma migrate dev --name create_user_table
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,18 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import http from "http"
+import { prismaClient } from "./lib/db";
 
+interface User {
+    newUser: {
+        firtName: string;
+        lastName: string;
+        email: string;
+        password: string;
+    }
+}
 
-async function init(){
+async function init() {
     const app = express()
     const PORT = process.env.PORT || 4000
     const httpServer = http.createServer(app);
@@ -15,24 +24,51 @@ async function init(){
             hello:String
             say(name: String): String
           }
+          
+          type Mutation {
+            createUser(newUser: InputUser!): Boolean
+          }
+
+          input InputUser {
+            firtName: String!
+            lastName: String!
+            email: String!
+            password: String!
+            salt: String! 
+          }
         `,
         resolvers: {
-            Query:{
-                hello : ()=> "Hello i am graphql",
-                say: (_,{name}:{name: string}) => `Hey ${name} how are you`
+            Query: {
+                hello: () => "Hello i am graphql",
+                say: (_, { name }: { name: string }) => `Hey ${name} how are you`
+            },
+            Mutation: {
+                createUser: async (_, { newUser }: User) => {
+                    const { firtName, lastName, email, password } = newUser
+                    await prismaClient.user.create({
+                        data: {
+                          firtName,
+                          lastName,
+                          email,
+                          password,
+                          salt : "testing_salt"
+                        }
+                    })
+                    return true
+                }
             }
         },
         plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     });
-    
+
     await server.start();
-    
+
     app.use(express.json())
     app.use(
         '/graphql',
         expressMiddleware(server),
-      );
- 
+    );
+
     app.listen(PORT, () => {
         console.log(`server is running on ${PORT}`)
     })
